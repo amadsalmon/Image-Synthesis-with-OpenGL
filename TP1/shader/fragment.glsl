88a3f63d580234a08106c5561cf0e1553d9eb526
Amad Salmon <amad.salmon@icloud.com>
2021-01-20 15:43:13 +0100

    Create fragment.glsl

---
+++ TP1/shader/fragment.glsl
@@ -0,0 +1,11 @@
+// Version d’OpenGL
+
+#version 120
+
+// Fonction appelee pour chaque fragment
+void main() {
+
+    // Affectation de la couleur du fragment
+    gl_FragColor = vec4 (1 .,0 .,0 .,1 .);
+
+}
