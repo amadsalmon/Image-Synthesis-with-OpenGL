9671166228b7525df67c4f4f36456d27edce7674
Amad Salmon <amad.salmon@etu.univ-grenoble-alpes.fr>
2021-02-28 13:43:55 +0100

    Update vertex.glsl

--- TP4/shader/vertex.glsl
+++ TP4/shader/vertex.glsl
@@ -20,4 +20,26 @@ void main() {
   // gl_Position est defini par defaut dans GLSL
   gl_Position = ProjectionMatrix * ViewMatrix * ModelMatrix * vec4(in_position, 1.0);
 
+  vec3 L_f;                   // Couleur résultante calculée par le modèle de Phong
+  float rho_a, rho_d, rho_s;  // Coefficients d'atténuation associés à chaque composante
+  vec3 L_a, L_d, L_s;         // Couleurs de chaque composante
+  vec3 n;                     // Vecteur normal de la surface au point p
+  vec3 l;                     // Vecteur de norme  dans la direction de la lumière
+  vec3 r;                     // Vecteur réflection de l par rapport à n : r=l-2<n,l>n
+  vec3 e;                     // Vecteur de norme 1 dans la direction entre le point p de la surface et la position c de l'oeil
+  float s;                    // Brillance (shininess)
+
+  vec4 posInObjectSpace = vec4( in_position, 1.0);
+  vec4 posInWorldSpace = ModelMatrix * posInObjectSpace;
+
+  r = reflect(l, n);
+
+  rho_s = 1.0f;
+  L_s = vec3(1.0, 1.0, 1.0);
+
+  L_f = rho_s*L_s*max(dot(r,e), 0);
+
+  //my_color = vec4(in_normal*0.5+0.5,1.0);
+  //my_color = posInWorldSpace+0.5;
+  my_color = vec4(L_f,1.0);
 }
